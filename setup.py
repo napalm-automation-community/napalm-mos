d5bbc196355d406f56f56a55ddbaeb0046323171
Brandon Ewing <brandon.ewing@warningg.com>
2019-10-11 09:07:14 -0500

    Update version to 2.5.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fs:
 
 setup(
     name="napalm-mos",
-    version="2.4.1",
+    version="2.5.0",
     packages=find_packages(),
     author="Benny Holmgren, Brandon Ewing",
     author_email="benny@holmgren.id.au, brandon.ewing@warningg.com",
